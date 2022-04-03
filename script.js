e80f8408fe8da2f7ace6d83ab4714d4858e2ff1f
CaioVitor1 <caiovitor33@hotmail.com>
2022-04-03 01:22:24 -0300

    finalização dos requisitos

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let prato;
 let bebida;
 let sobremesa;
+let precoPrato;
+let precoBebida;
+let precoSobremesa;
+let preco = precoPrato + precoBebida + precoSobremesa
 
 function escolherPrato1() {
     let frango1 = document.querySelector(".item1");
@@ -15,7 +19,20 @@ function escolherPrato1() {
     ok3.classList.add("desligar3");
     frango1.classList.add("bordas");
     frango2.classList.remove("bordas");
-    frango3.classList.remove("bordas");   
+    frango3.classList.remove("bordas");  
+    
+    prato = "frango1"
+    precoPrato = 14.90
+
+    if ( (prato === "frango1" || prato === "frango2" || prato === "frango3") && (bebida === "bebida1" || bebida === "bebida2" || bebida === "bebida3") && (sobremesa === "sobremesa1" || sobremesa === "sobremesa2" || sobremesa === "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
+    
 }
 function escolherPrato2() {
     let frango1 = document.querySelector(".item1");
@@ -30,7 +47,20 @@ function escolherPrato2() {
     ok3.classList.add("desligar3");
     frango1.classList.remove("bordas");
     frango2.classList.add("bordas");
-    frango3.classList.remove("bordas");   
+    frango3.classList.remove("bordas"); 
+    
+    prato = "frango2"
+    precoPrato = 16.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
+
 }
 function escolherPrato3() {
     let frango1 = document.querySelector(".item1");
@@ -46,6 +76,18 @@ function escolherPrato3() {
     frango1.classList.remove("bordas");
     frango2.classList.remove("bordas");
     frango3.classList.add("bordas");   
+
+    prato = "frango3"
+    precoPrato = 19.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
 
  }
 /* Escolhendo a bebida */
@@ -64,6 +106,19 @@ function escolherBebida1() {
     bebida1.classList.add("bordas");
     bebida2.classList.remove("bordas");
     bebida3.classList.remove("bordas");   
+
+    bebida = "bebida1"
+    precoBebida = 4.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
+
 }
 function escolherBebida2() {
     let bebida1 = document.querySelector(".item4");
@@ -79,6 +134,19 @@ function escolherBebida2() {
     bebida1.classList.remove("bordas");
     bebida2.classList.add("bordas");
     bebida3.classList.remove("bordas");   
+
+    bebida = "bebida2"
+    precoBebida = 5.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
+
 }
 function escolherBebida3() {
     let bebida1 = document.querySelector(".item4");
@@ -94,6 +162,18 @@ function escolherBebida3() {
     bebida1.classList.remove("bordas");
     bebida2.classList.remove("bordas");
     bebida3.classList.add("bordas");   
+
+    bebida = "bebida3"
+    precoBebida = 7.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
 
  }
 
@@ -113,6 +193,19 @@ function escolherBebida3() {
     sobremesa1.classList.add("bordas");
     sobremesa2.classList.remove("bordas");
     sobremesa3.classList.remove("bordas");   
+
+    sobremesa = "sobremesa1"
+    precoSobremesa = 7.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
+
 }
 function escolherSobremesa2() {
     let sobremesa1 = document.querySelector(".item7");
@@ -128,6 +221,19 @@ function escolherSobremesa2() {
     sobremesa1.classList.remove("bordas");
     sobremesa2.classList.add("bordas");
     sobremesa3.classList.remove("bordas");   
+
+    sobremesa = "sobremesa2"
+    precoSobremesa = 8.90
+
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
+
 }
 function escolherSobremesa3() {
     let sobremesa1 = document.querySelector(".item7");
@@ -142,8 +248,38 @@ function escolherSobremesa3() {
     ok3.classList.remove("desligar9");
     sobremesa1.classList.remove("bordas");
     sobremesa2.classList.remove("bordas");
-    sobremesa3.classList.add("bordas");   
+    sobremesa3.classList.add("bordas");  
+
+    sobremesa = "sobremesa3"
+    precoSobremesa = 10.90
+    
+    if ( (prato == "frango1" || prato == "frango2" || prato == "frango3") && (bebida == "bebida1" || bebida == "bebida2" || bebida == "bebida3") && (sobremesa == "sobremesa1" || sobremesa == "sobremesa2" || sobremesa == "sobremesa3") ) {
+        let pedido =  document.querySelector(".texto-pedido");
+        let cor = document.querySelector(".finalizar-pedido")
+
+        pedido.innerHTML = "Fechar pedido"
+        pedido.classList.add("texto-cor")
+        cor.classList.add("verde");
+    }
  }
 
+
+  /*
+  function fazerPedido() {
+    let mensagem = "olá, gostaria de fazer o pedido: - Prato: " + prato + "- Bebida: " + bebida + "- Sobremesa: " + sobremesa + " Total: R$ 30.00"
+    mensagem = window.encodeURIComponent(mensagem);
+
+    window.open("https://wa.me/5586994846600?text=" + mensagem)
+}
+  */
+
+function fazerPedido() {
+    let mensagem = `Olá, gostaria de fazer o pedido:
+    - Prato: ${prato}
+    - Bebida: ${bebida}
+    - Sobremesa: ${sobremesa}
+    - Total: ${(precoPrato + precoBebida + precoSobremesa).toFixed(2)} ` 
+    window.open("https://wa.me/5586994846600?text=" + mensagem)
+}
 
 
